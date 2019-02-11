bd912dc98f41519ce418973d0045ef73a1ece311
Rogerio Motitsuki <rogerio@amlight.net>
2019-02-11 14:26:53 -0200

    Fix test sending wrong parameter to handle_link_up

--- tests/models/test_link_protection.py
+++ tests/models/test_link_protection.py
@@ -443,7 +443,7 @@ class TestLinkProtection(TestCase):  # pylint: disable=too-many-public-methods
 
         evc = EVC(**attributes)
         evc.current_path = Path([])
-        current_handle_link_up = evc.handle_link_up(backup_path[0].endpoint_a)
+        current_handle_link_up = evc.handle_link_up(backup_path[0])
         self.assertEqual(deploy_mocked.call_count, 0)
         self.assertEqual(deploy_to_mocked.call_count, 1)
         deploy_to_mocked.assert_called_once_with('backup_path',
