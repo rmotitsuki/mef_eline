9eedba0e8501a47eaea3c7b52650f35423667638
Italo Valcy <italo@ampath.net>
2022-02-11 11:50:39 -0300

    better description of the WAIT_FOR_OLD_PATH setting parameter

--- settings.py
+++ settings.py
@@ -15,7 +15,9 @@ SDN_TRACE_CP_URL = "http://localhost:8181/api/amlight/sdntrace_cp"
 # EVC consistency interval
 DEPLOY_EVCS_INTERVAL = 60
 
-# Time to wait for old path to become available
+# Number of execution rounds to wait for old path to become available.
+# i.e., it will wait up to DEPLOY_EVCS_INTERVAL*WAIT_FOR_OLD_PATH to the old
+# path become available, otherwise mef_eline consistency will redeploy it
 WAIT_FOR_OLD_PATH = 5
 
 # Prefix this NApp has when using cookies
