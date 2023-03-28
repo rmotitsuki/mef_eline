b796d4a19db3d047a4bdd2f98fc728121c01a46e
Vinicius Arcanjo <vindasil@fiu.edu>
2023-03-28 15:28:36 -0300

    chore: nuked load_spec and validate decorator from utils.py

--- utils.py
+++ utils.py
@@ -1,16 +1,4 @@
 """Utility functions."""
-import functools
-from pathlib import Path
-
-from flask import request
-from openapi_core.contrib.flask import FlaskOpenAPIRequest
-from openapi_core.spec.shortcuts import create_spec
-from openapi_core.validation.request import openapi_request_validator
-from openapi_spec_validator import validate_spec
-from openapi_spec_validator.readers import read_from_filename
-from werkzeug.exceptions import BadRequest, UnsupportedMediaType
-
-from kytos.core import log
 from kytos.core.events import KytosEvent
 
 
@@ -66,65 +54,3 @@ def compare_uni_out_trace(uni, trace):
         uni.interface.port_number == trace["out"].get("port")
         and uni_vlan == trace["out"].get("vlan")
     )
-
-
-def load_spec():
-    """Validate openapi spec."""
-    napp_dir = Path(__file__).parent
-    yml_file = napp_dir / "openapi.yml"
-    spec_dict, _ = read_from_filename(yml_file)
-
-    validate_spec(spec_dict)
-
-    return create_spec(spec_dict)
-
-
-def validate(spec):
-    """Decorator to validate a REST endpoint input.
-
-    Uses the schema defined in the openapi.yml file
-    to validate.
-    """
-
-    def validate_decorator(func):
-        @functools.wraps(func)
-        def wrapper_validate(*args, **kwargs):
-            try:
-                data = request.get_json()
-            except BadRequest:
-                result = "The request body is not a well-formed JSON."
-                log.debug("create_circuit result %s %s", result, 400)
-                raise BadRequest(result) from BadRequest
-            if data is None:
-                result = "The request body mimetype is not application/json."
-                log.debug("update result %s %s", result, 415)
-                raise UnsupportedMediaType(result)
-
-            openapi_request = FlaskOpenAPIRequest(request)
-            result = openapi_request_validator.validate(spec, openapi_request)
-            if result.errors:
-                error_response = (
-                    "The request body contains invalid API data."
-                )
-                errors = result.errors[0]
-                if hasattr(errors, "schema_errors"):
-                    schema_errors = errors.schema_errors[0]
-                    error_log = {
-                        "error_message": schema_errors.message,
-                        "error_validator": schema_errors.validator,
-                        "error_validator_value": schema_errors.validator_value,
-                        "error_path": list(schema_errors.path),
-                        "error_schema": schema_errors.schema,
-                        "error_schema_path": list(schema_errors.schema_path),
-                    }
-                    log.debug("Invalid request (API schema): %s", error_log)
-                    error_response += f" {schema_errors.message} for field"
-                    error_response += (
-                        f" {'/'.join(map(str,schema_errors.path))}."
-                    )
-                raise BadRequest(error_response) from BadRequest
-            return func(*args, data=data, **kwargs)
-
-        return wrapper_validate
-
-    return validate_decorator
